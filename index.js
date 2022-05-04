1a0f6ff2ca10b7104f44c09d66e161faf560f5eb
Sydney <sydneysirois17@gmail.com>
2022-05-03 21:53:02 -0400

    all tests pass except true

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const validateLineup = (lineup) => {
-  let currentLineup = { ...lineup }
-
-
-  if (playerPosition() && playerCost() && playerTeam() && playerGame()) return // true or false
+  if (playerPosition(lineup) && playerCost(lineup) && playerTeam(lineup) && playerGame(lineup)) {
+    return true
+  } else return false
 }
 
 const playerCost = (lineup) => {
@@ -25,74 +24,111 @@ const playerPosition = (lineup) => {
   if (OF && pitch && first && second && third && catchers && short) {
     return true
   }
+
+  return false
 }
 
 const outfield = (lineup) => {
   const outfieldplayers = lineup.filter((player) => player.position === 'OF')
-  
+
   if (outfieldplayers.length !== 3) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const pitcher = (lineup) => {
   const pitcherplayers = lineup.filter((player) => player.position === 'P')
-  
+
   if (pitcherplayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const firstBase = (lineup) => {
   const firstBaseplayers = lineup.filter((player) => player.position === '1B')
-  
+
   if (firstBaseplayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const secondBase = (lineup) => {
   const secondBaseplayers = lineup.filter((player) => player.position === '2B')
-  
+
   if (secondBaseplayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const thirdBase = (lineup) => {
   const thirdBaseplayers = lineup.filter((player) => player.position === '3B')
-  
+
   if (thirdBaseplayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const catcher = (lineup) => {
   const catcherplayers = lineup.filter((player) => player.position === 'C')
-  
+
   if (catcherplayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 const shortStop = (lineup) => {
   const shortStopPlayers = lineup.filter((player) => player.position === 'SS')
-  
+
   if (shortStopPlayers.length !== 1) {
     return false
-  } 
+  }
 
+  return true
 }
 
 
 const playerTeam = (lineup) => {
-  return
+  const uniqueTeams = [...new Set(lineup.map(player => player.teamId))]
+
+  for (let i = 0; i < uniqueTeams.length; i++) {
+    const currentTeam = uniqueTeams[i]
+
+    const currentLineup = lineup.filter((player) => player.teamId === currentTeam).length
+
+    if (currentLineup > 2) {
+      return false
+    }
+  }
+
+  return true
+}
+
+const playerGame = (lineup) => {
+  const uniqueTeams = [...new Set(lineup.map(player => player.gameId))]
+
+    for (let i = 0; i < uniqueTeams.length; i++) {
+    const currentTeam = uniqueTeams[i]
+
+    const currentLineup = lineup.filter((player) => player.gameId === currentTeam).length
+
+    if (currentLineup > 2) {
+      return false
+    }
+  }
+
+  return true
 }
 
 
